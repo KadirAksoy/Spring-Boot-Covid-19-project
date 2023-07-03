be9056ab60bb6457a68a26c561dda7b15aa2ad84
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-07-03 10:57:14 +0300

    Initial commit

---
+++ src/main/java/com/kadiraksoy/Spring/Boot/Covid19/project/SpringBootCovid19ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.Spring.Boot.Covid19.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCovid19ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCovid19ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadiraksoy/Spring/Boot/Covid19/project/SpringBootCovid19ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.Spring.Boot.Covid19.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCovid19ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
